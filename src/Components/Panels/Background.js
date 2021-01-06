7bcae93db94a1d7820b4049744edb5156a6044e7
Andres-XL <amovilla1103@gmail.com>
2021-01-06 02:14:07 -0500

    background has spacing now

--- src/Components/Panels/Background.js
+++ src/Components/Panels/Background.js
@@ -1,9 +1,9 @@
-import React, { Fragment } from 'react';
+import React  from 'react';
 
 export function Background( { player_data, player_index } ) {
     return (
-        <Fragment>
+        <div className={'bkg-card'}>
             {player_data[player_index].player_data.background}
-        </Fragment>
+        </div>
     );
 }
